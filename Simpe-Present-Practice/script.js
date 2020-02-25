efc91cec4377c7c9b588dc24fdc280771982a20e
Nicolas Garcia <60039467+NicolasGarcia613@users.noreply.github.com>
2020-02-25 13:11:54 +0000

    Add files via upload

---
+++ Simpe-Present-Practice/script.js
@@ -0,0 +1,15 @@
+// jQuery
+2
+$('[name=tags]').tagify();
+3
+ 
+4
+// Vanilla JavaScript
+5
+var input = document.querySelector('input[name=tags]'),
+6
+tagify =new Tagify( input );
+
+    }
+
+    $('[name=tags]').tagify({duplicates :false});
